175dbf21d89ff220d341d775f7039fad4b538c29
narapady <chhuoynarapady@yahoo.com>
2022-07-12 14:03:48 -0700

    add dotenv

--- ingest/uploader.py
+++ ingest/uploader.py
@@ -1,15 +1,14 @@
 import boto3
 import os
-from aws_access_key import ACCESS_KEY_ID, SECRET_ACCESS_KEY
+from dotenv import load_dotenv
 
-client = boto3.client('s3', aws_access_key_id= ACCESS_KEY_ID, aws_secret_access_key=SECRET_ACCESS_KEY)
+
+load_dotenv()
+
+client = boto3.client('s3', 
+                       aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
+                       aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"))
 client.create_bucket(Bucket="s3-bucket-raw-usda-ers")
-
-for filename in os.listdir():
-    if "xls" in filename:
-        with open(filename, "rb") as file:
-            key = "test-xsl/" + filename
-            client.upload_fileobj(file,"s3-bucket-raw-usda-ers", key)
 
 for dir in os.listdir():
     if "-" in dir:
