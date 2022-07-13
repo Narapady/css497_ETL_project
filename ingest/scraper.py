367c25f35b0e299113198effd4595e6c2a70c8ec
narapady <chhuoynarapady@yahoo.com>
2022-07-12 17:46:31 -0700

    worked on kaggle upload

--- ingest/scraper.py
+++ ingest/scraper.py
@@ -1,27 +1,33 @@
 import requests
 import os
 import boto3
+import zipfile
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
-def to_lowercase(word):
+def to_lowercase(word: str) -> str:
     result = word.split(" ")
     result = [item.lower() for item in result]
     return " ".join(result).replace(" ", "-")
 
-def upload_to_s3(urls_dict: dict[str, list[str]], bucket_name: str) -> None:
-
+def create_s3_bucket(bucket_name: str):
     client = boto3.client('s3', 
                        aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
                        aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"))
-    client.create_bucket(Bucket="s3-bucket-raw-usda-ers")
-    
+    client.create_bucket(Bucket=bucket_name)
+    return client
+
+
+def upload_to_s3(urls_dict: dict[str, list[str]], bucket_name: str) -> None:
+
+    s3 = create_s3_bucket(bucket_name=bucket_name) 
+
     for key, values in urls_dict.items():
         key = to_lowercase(key)        
         for link in values:
             req = requests.get(link)
             filename = req.url[link.rfind("/") + 1: link.rfind("?")]
-            client.put_object( 
+            s3.put_object( 
                 Bucket= bucket_name,
                 Body= req.content,
                 Key=  key + "/" + filename
@@ -50,6 +56,32 @@ def get_links(url: str) -> dict[str, list[str]]:
 
     return links
 
+def kaggle_upload():
+    # api_commands = ["kaggle datasets download -d amanarora/obesity-among-adults-by-country-19752016",
+    #                 "kaggle datasets download -d annedunn/obesity-and-gdp-rates-from-50-states-in-20142017",
+    #                 "kaggle datasets download -d spittman1248/cdc-data-nutrition-physical-activity-obesity"]
+
+    api_commands = ["kaggle datasets download -d amanarora/obesity-among-adults-by-country-19752016"]
+    s3 = create_s3_bucket("s3-bucket-raw-kaggle") 
+    for command in api_commands:
+        os.system(command)
+        filename = command.split("/")[1]
+        with zipfile.ZipFile(filename + ".zip") as zip:
+            # filenames = zipfilet()
+            zip.extractall()
+            
+            for file in os.listdir():
+                if ".csv" in file:
+                    s3.put_object( 
+                        Bucket= "s3-bucket-raw-kaggle",
+                        Body= file,
+                        Key=  "kaggle-data" + "/" + file
+                    )    
+
+
+
+
+
 if __name__ == "__main__":
     urls = [
         "https://www.ers.usda.gov/data-products/commodity-consumption-by-population-characteristics/",
@@ -66,5 +98,5 @@ if __name__ == "__main__":
     url = urls[-1]
     links = get_links(url)
     # download_xls(links)
-    upload_to_s3(links, "s3-bucket-raw-usda-ers")
-    
+    # upload_to_s3(links, "s3-bucket-raw-usda-ers")
+    kaggle_upload()    
