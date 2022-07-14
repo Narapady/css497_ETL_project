2f68967def2e96ccceef89dcd03d21cd10770e05
narapady <chhuoynarapady@yahoo.com>
2022-07-13 18:15:41 -0700

    refactored ingestor

--- ingest/ingestor.py
+++ ingest/ingestor.py
@@ -4,20 +4,11 @@ import boto3
 import zipfile
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-from typing import Callable
-from abc import ABC, abstractmethod
 
 def to_lowercase(word: str) -> str:
     result = word.split(" ")
     result = [item.lower() for item in result]
     return " ".join(result).replace(" ", "-")
-
-def create_s3_bucket(bucket_name: str):
-    client = boto3.client('s3', 
-                       aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
-                       aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"))
-    client.create_bucket(Bucket=bucket_name)
-    return client
 
 def get_links(url: str) -> dict[str, list[str]]: 
     """
@@ -42,66 +33,54 @@ def get_links(url: str) -> dict[str, list[str]]:
 
     return links
 
-def upload_ers_to_s3(sources: list[str], bucket_name: str) -> None:
-    
-    source_list = [get_links(source) for source in sources]
-    s3 = create_s3_bucket(bucket_name=bucket_name) 
+class S3AWS:
+    def __init__(self, access_key_id, secret_access_key):
+        self.access_key_id = access_key_id
+        self.secret_access_key = secret_access_key
+        self.bucket_list = []
 
-    for urls_dict in source_list: 
-        for key, values in urls_dict.items():
-            key = to_lowercase(key)        
-            for link in values:
-                req = requests.get(link)
-                filename = req.url[link.rfind("/") + 1: link.rfind("?")]
-                s3.put_object( 
-                    Bucket= bucket_name,
-                    Body= req.content,
-                    Key=  key + "/" + filename
-                )    
+    def create_bucket(self, bucket_name: str):
+        client = boto3.client('s3', 
+                           aws_access_key_id=self.access_key_id,
+                           aws_secret_access_key=self.secret_access_key)
+        client.create_bucket(Bucket=bucket_name)
+        self.bucket_list.append(bucket_name)
 
-def upload_kaggle_to_s3(sources: list[str], bucket_name: str) -> None:
+        return client
 
-    s3 = create_s3_bucket(bucket_name) 
-    for api_command in sources:
-        os.system(api_command)
-        filename = api_command.split("/")[1]
-        with zipfile.ZipFile(filename + ".zip") as zip:
-            zip.extractall()
-            for file in os.listdir():
-                if ".csv" in file:
-                    s3.upload_file(file, bucket_name, "obesity/" + file)
-            
-        os.system("rm *.zip *.csv")
+    def ingest_from_usda(self, sources: list[str], bucket_name: str) -> None:
+        source_list = [get_links(source) for source in sources]
+        s3 = self.create_bucket(bucket_name=bucket_name) 
 
-# def delete_all_s3_buckets():
-#     client = boto3.client('s3', 
-#                        aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
-#                        aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"))
-#     s3 = boto3.resource('s3',
-#                        aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
-#                        aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"))
+        for urls_dict in source_list: 
+            for key, values in urls_dict.items():
+                key = to_lowercase(key)        
+                for link in values:
+                    req = requests.get(link)
+                    filename = req.url[link.rfind("/") + 1: link.rfind("?")]
+                    s3.put_object( 
+                        Bucket= bucket_name,
+                        Body= req.content,
+                        Key=  key + "/" + filename
+                    )    
 
-#     buckets = client.list_buckets()
+    def ingest_from_kaggle(self, sources: list[str], bucket_name: str) -> None:
+        s3 = self.create_bucket(bucket_name) 
+        for api_command in sources:
+            os.system(api_command)
+            filename = api_command.split("/")[1]
+            with zipfile.ZipFile(filename + ".zip") as zip:
+                zip.extractall()
+                for file in os.listdir():
+                    if ".csv" in file:
+                        s3.upload_file(file, bucket_name, "obesity/" + file)
+                
+            os.system("rm *.zip *.csv")
 
-#     for bucket in buckets['Buckets']:
-#         s3_bucket = s3.Bucket(bucket['Name'])
-#         s3_bucket.objects.all().delete()
-#         s3_bucket.delete()
 def auth_credentials():
     load_dotenv()
 
 class Ingestor():
-    def __init__(self, sources: list[str], destination: str):
-        self.sources = sources
-        self.destination = destination 
-
-    def ingest_data(self, ingest_func: Callable[[list[str], str],None]):
-        ingest_func(self.sources, self.destination) 
-
-    
-if __name__ == "__main__":
-    auth_credentials() 
-
     kaggle_sources = ["kaggle datasets download -d amanarora/obesity-among-adults-by-country-19752016"]
                     # "kaggle datasets download -d annedunn/obesity-and-gdp-rates-from-50-states-in-20142017",
                     # "kaggle datasets download -d spittman1248/cdc-data-nutrition-physical-activity-obesity"]    
@@ -113,12 +92,23 @@ if __name__ == "__main__":
     #     "https://www.ers.usda.gov/data-products/food-price-outlook/",
     #     "https://www.ers.usda.gov/data-products/food-expenditure-series/"
     # ]
+
+    def __init__(self, source: str, s3: S3AWS):
+        self.source = source
+        self.s3 = s3 
+
+    def ingest_data(self):
+        if self.source == "kaggle":
+            s3.ingest_from_kaggle(self.kaggle_sources, "s3-bucket-raw-kaggle")
+        elif self.source == "usda":
+            s3.ingest_from_usda(self.usda_sources, "s3-bucket-raw-usda")
+        
+if __name__ == "__main__":
+    auth_credentials() 
     
-    # upload_ers_to_s3(urls, "s3-bucket-raw-usda-ers")
-    # upload_kaggle_to_s3(api_commands, "s3-bucket-raw-kaggle")
-    usda_sources  = ["https://www.ers.usda.gov/data-products/food-expenditure-series/"]
-    ingestor_usda = Ingestor(usda_sources, "s3-bucket-raw-usda-ers")
-    ingestor_usda.ingest_data(upload_ers_to_s3)
+    s3 = S3AWS(os.getenv("ACCESS_KEY_ID"),os.getenv("SECRET_ACCESS_KEY"))
+    ingestor_kaggle = Ingestor("kaggle", s3)
+    ingestor_kaggle.ingest_data()
 
 
 
