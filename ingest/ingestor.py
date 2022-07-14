437e229b3b0a3ef22ea4e519b429ff47575a4d70
narapady <chhuoynarapady@yahoo.com>
2022-07-14 16:35:57 -0700

    final commit

--- ingest/ingestor.py
+++ ingest/ingestor.py
@@ -1,7 +1,62 @@
 import os
+import requests
+import util
+import zipfile
 from dotenv import load_dotenv
+from typing import Callable
+from dataclasses import dataclass, field
 from s3 import S3AWS 
 
+def ingest_usda(sources: list[str], bucket_name: str, s3aws: S3AWS) -> None:
+    """
+    Ingest data from USDA ERS to specified S3 buckets. 'sources' paramenter
+    is the website links that contain data to ingest. 
+    """
+    source_list = [util.get_links(source) for source in sources]
+    s3 = s3aws.create_bucket(bucket_name=bucket_name) 
+
+    for urls_dict in source_list: 
+        for key, values in urls_dict.items():
+            key = util.to_lowercase(key)        
+            for link in values:
+                req = requests.get(link)
+                filename = req.url[link.rfind("/") + 1: link.rfind("?")]
+                s3.put_object( 
+                    Bucket= bucket_name,
+                    Body= req.content,
+                    Key=  key + "/" + filename
+                )    
+
+def ingest_kaggle(sources: list[str], bucket_name: str, s3aws: S3AWS) -> None:
+    """
+    Ingest data from Kaggle to specified S3 bucket via Kaggle API commnads.
+    'sources' paramenter is a list of kaggle api commands to get the data. 
+    """
+    s3 = s3aws.create_bucket(bucket_name) 
+    for api_command in sources:
+        os.system(api_command)
+        filename = api_command.split("/")[1]
+        with zipfile.ZipFile(filename + ".zip") as zip:
+            zip.extractall()
+            for file in os.listdir():
+                if ".csv" in file:
+                    s3.upload_file(file, bucket_name, "obesity/" + file)
+            
+    os.system("rm *.zip *.csv")
+
+def aws_credentials() -> None:
+    """
+    load aws s3 credentials to gain access to S3 
+    """
+    load_dotenv()
+
+"""
+Type alias for ingesting stragegy. Any callable object of this
+type can be bass as parameter to Ingestor.ingest() method
+"""
+ingestStrategy = Callable[[list[str], str, S3AWS], None]
+
+@dataclass
 class Ingestor:
     """
     Ingestor class is repsonsible for ingesting data from source system with
@@ -9,6 +64,24 @@ class Ingestor:
     source to ingest data from, aws s3 object, and the name of s3 bucket to store the data.
 
     """
+    s3: S3AWS
+    bucket_name: str
+    sources: list[str] = field(default_factory=list[str]) 
+    
+    def ingest(self, ingest_strategy: ingestStrategy) -> None:
+        """
+        ingesting data to s3 bucket based on ingesting strategy
+        """
+        ingest_strategy(self.sources, self.bucket_name, self.s3)
+
+def main() -> None:
+    """
+    Main function of ingestion program. One aws s3 is instantiated, and
+    ingest data from 2 sources systems including data from usda and kaggle. 
+    Data will land in S3 with specified bucket names. 
+    """
+    aws_credentials() 
+    
     kaggle_sources = ["kaggle datasets download -d amanarora/obesity-among-adults-by-country-19752016",
                       "kaggle datasets download -d annedunn/obesity-and-gdp-rates-from-50-states-in-20142017",
                       "kaggle datasets download -d spittman1248/cdc-data-nutrition-physical-activity-obesity"
@@ -22,48 +95,18 @@ class Ingestor:
         "https://www.ers.usda.gov/data-products/food-price-outlook/",
         "https://www.ers.usda.gov/data-products/food-expenditure-series/"
     ]
-
-    def __init__(self, source: str, bucket_name: str, s3: S3AWS):
-        self.source = source
-        self.bucket_name = bucket_name
-        self.s3 = s3
-    
-    def ingest_data(self) -> None:
-        """
-        Executiong ingesting or extracting data to s3 bucket based on name of the source
-        passed in the instantiation. 
-        """
-        if self.source == "kaggle":
-            self.s3.ingest_from_kaggle(self.kaggle_sources, self.bucket_name)
-        elif self.source == "usda":
-            self.s3.ingest_from_usda(self.usda_sources, self.bucket_name)
-
-def aws_credentials() -> None:
-    """
-    load aws s3 credentials to gain access to S3 
-    """
-    load_dotenv()
-
-def main() -> None:
-    """
-    Main function of ingestion program. One aws s3 is instantiated, and
-    ingest data from 2 sources systems including data from usda and kaggle. 
-    Data will land in S3 with specified bucket names. 
-    """
-    aws_credentials() 
     
     s3 = S3AWS(os.getenv("ACCESS_KEY_ID"),os.getenv("SECRET_ACCESS_KEY"))
 
-    ingestor_kaggle = Ingestor("kaggle", "s3-bucket-raw-kaggle", s3)
-    ingestor_kaggle.ingest_data()
+    ingestor_kaggle = Ingestor(s3, "s3-bucket-raw-kaggle", kaggle_sources)
+    ingestor_kaggle.ingest(ingest_kaggle)
 
-    ingestor_usda = Ingestor("usda", "s3-bucket-raw-usda", s3)
-    ingestor_usda.ingest_data()
+    ingestor_usda = Ingestor(s3, "s3-bucket-raw-usda", usda_sources)
+    ingestor_usda.ingest(ingest_usda)
 
+# main program
 if __name__ == "__main__":
     main()
-
-
 
 
 
--- ingest/s3.py
+++ ingest/s3.py
@@ -1,15 +1,10 @@
-import requests
-import os
 import boto3
-import zipfile
 from typing import Optional
-import util
 
 class S3AWS:
     """
     S3AWS represents aws object storage s3. It handles necessary features including
-    authenticating to access to s3, creating bucket, and ingest data from different
-    sources to S3.
+    authenticating to access to s3, and creating bucket to store the ingested data
     """
     def __init__(self, access_key_id: Optional[str], secret_access_key: Optional[str]):
         self.access_key_id = access_key_id
@@ -28,40 +23,4 @@ class S3AWS:
 
         return client
 
-    def ingest_from_usda(self, sources: list[str], bucket_name: str) -> None:
-        """
-        Ingest data from USDA ERS to specified S3 buckets. 'sources' paramenter
-        is the website links that contain data to ingest. 
-        """
-        source_list = [util.get_links(source) for source in sources]
-        s3 = self.create_bucket(bucket_name=bucket_name) 
-
-        for urls_dict in source_list: 
-            for key, values in urls_dict.items():
-                key = util.to_lowercase(key)        
-                for link in values:
-                    req = requests.get(link)
-                    filename = req.url[link.rfind("/") + 1: link.rfind("?")]
-                    s3.put_object( 
-                        Bucket= bucket_name,
-                        Body= req.content,
-                        Key=  key + "/" + filename
-                    )    
-
-    def ingest_from_kaggle(self, sources: list[str], bucket_name: str) -> None:
-        """
-        Ingest data from Kaggle to specified S3 bucket via Kaggle API commnads.
-        'sources' paramenter is a list of kaggle api commands to get the data. 
-        """
-        s3 = self.create_bucket(bucket_name) 
-        for api_command in sources:
-            os.system(api_command)
-            filename = api_command.split("/")[1]
-            with zipfile.ZipFile(filename + ".zip") as zip:
-                zip.extractall()
-                for file in os.listdir():
-                    if ".csv" in file:
-                        s3.upload_file(file, bucket_name, "obesity/" + file)
-                
-        os.system("rm *.zip *.csv")
 
