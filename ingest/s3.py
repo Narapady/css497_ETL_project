af515a830548e15f4a0101ced6c42e308abec13b
narapady <chhuoynarapady@yahoo.com>
2022-07-22 17:13:11 -0700

    work on staging

--- ingest/s3.py
+++ ingest/s3.py
@@ -1,6 +1,11 @@
 import boto3
+import os
+from dotenv import load_dotenv
+from smart_open import smart_open
 from typing import Optional
 from dataclasses import dataclass
+import pandas as pd
+import numpy as np
 
 @dataclass
 class S3AWS:
@@ -20,9 +25,16 @@ class S3AWS:
         """
         client = boto3.client('s3', 
                            aws_access_key_id=self.access_key_id,
-                           aws_secret_access_key=self.secret_access_key)
+                           aws_secret_access_key=self.secret_key_id)
         client.create_bucket(Bucket=bucket_name)
 
         return client
 
+    def load_df(self, bucket_name: str, key: str, type: str, sheet: int):
+        path = f"s3://{self.access_key_id}:{self.secret_key_id}@{bucket_name}/{key}"
+        
+        if type == "csv":
+            return pd.read_csv(smart_open(path))
+        elif type == "xls":
+            return pd.read_excel(path, sheet)
 
